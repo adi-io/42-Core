a43a01463de88bfa58c914f3f84941c7491ae094
Martin Man <mman@c3r2s1.42prague.com>
2024-06-01 17:16:09 +0200

    formatting fix add

--- minishell/minipwd.c
+++ minishell/minipwd.c
@@ -6,7 +6,7 @@
 /*   By: mman <mman@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 01:52:32 by mman              #+#    #+#             */
-/*   Updated: 2024/06/01 17:10:10 by mman             ###   ########.fr       */
+/*   Updated: 2024/06/01 17:14:41 by mman             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 //pwd without any options
 int	mini_pwd(t_tools *tools)
 {
-	printf("\n%s\n", tools->pwd);
+	printf("%s\n", tools->pwd);
 	return(EXIT_SUCCESS);
 }
